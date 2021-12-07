efb3b3f8eca74fd8a75dfebe48bf8588239c7d5d
localadmin@euro <localadmin@euro>
2021-12-07 11:23:06 +0000

    added basic transform logging

---
+++ Assets/ObjectLogger.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ObjectLogger : MonoBehaviour
+{
+    public string Directory;
+    public string Name;
+
+    private TextWriter writer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int postfix = 0;
+        string filename = "";
+        do
+        {
+            filename = Path.Combine(Directory, $"{Name}_{postfix}_{DateTime.Now.ToString("MM-dd-yyyy_H-mm-ss")}.csv");
+        } while (File.Exists(filename));
+        writer = new StreamWriter(filename);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        writer.WriteLine($"{DateTime.Now.Ticks} {transform.position.x} {transform.position.y} {transform.position.z} {transform.rotation.x} {transform.rotation.y} {transform.rotation.z} {transform.rotation.w}");
+        writer.Flush();
+    }
+
+    private void OnDestroy()
+    {
+        writer.Dispose();
+        writer = null;
+    }
+}
